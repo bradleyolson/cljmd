6d3eb40158787c80b930f4d9f169dafbacc6b0f9
Brad Olson <alllateralus@gmail.com>
2011-12-30 11:17:02 -0600

    sections builds... improperly. either need to not go 1 level deeper or replace items in the map; so I must go with the former.

--- src/cljmd/core.clj
+++ src/cljmd/core.clj
@@ -3,7 +3,7 @@
   (:use clojure.string))
 
 (def sectional-opts 
-  { :blockquote #"^[\ ]{0,1}\>"
+  { :blockquote #"^[\ ]{0,1}\>\ "
     :ol #"^[\ ]{0,1}[0-9]*\." 
   })
 
@@ -17,11 +17,9 @@
   (clojure.string/replace text pattern ""))
 
 (defn build-section
-  [lines tag]
-  (let [tag-str tag
-        tag-key (jmx/maybe-keywordize tag-str)]
-    (if (not-empty (and (rest lines) (re-find (sectional-opts tag-key) (first lines))))
-        (conj (build-section (rest lines) tag-str) (remove-md (first lines) (sectional-opts tag-key))))))
+  [lines tag] 
+  (if (not-empty (and (rest lines) (re-find (sectional-opts tag) (first lines))))
+      (conj (build-section (rest lines) tag) (remove-md (first lines) (sectional-opts tag)))))
 
 (defn test-opt
   [line opt]
@@ -30,28 +28,22 @@
 (defn test-blocks
   [line tag-opts]
   (for [opt tag-opts]
-    (if (not-empty (re-find (val opt) line)) (key opt)))
-  ;(let [foo (apply (fn [& opt] (toUpperCase (vals opt))) tag-opts)])
-  ;(apply (fn [f b] (println f) (println b)) sectional-opts)
-  ;(apply (fn [regex] (if (not-empty (re-find regex)) (println "foo"))) (vals opts))
-  ;(apply (fn [tag reg] (if (not-empty (re-find (reg line))) (println "foo"))) sectional-opts)
-  )
-
-(defn sectionalize
-  [lines]
-  (let [tag (first (keep identity (test-blocks (first lines) sectional-opts)))]
-    (if (keyword? tag)
-      (println (build-section lines tag)))
-    (if (not-empty (rest lines)) (recur (rest lines))) ))
+    (if (not-empty (re-find (val opt) line)) (key opt))))
 
 (defn list-to-text
   [text]
   (apply str (interpose "\r\n" text)))
 
+(defn sectionalize
+  [lines]
+  (let [tag (first (keep identity (test-blocks (first lines) sectional-opts)))]
+    (if (keyword? tag)
+      (println (build-tag (str tag) (list-to-text (build-section lines tag)))))
+    (if (not-empty (rest lines)) (recur (rest lines)))))
+
 (defn build-markup
   [text]
-  (let [lines (split-lines text)
-        sections (build-section lines "blockquote")]
+  (let [lines (split-lines text)]
     (println "run-1")
     (sectionalize lines)
     ;(println (build-tag "ol" (list-to-text (build-section lines "ol"))))
--- src/cljmd/test.clj
+++ src/cljmd/test.clj
@@ -7,7 +7,8 @@
 ###bazw#ibble\r\n# wobble
 #######flub")
 
-(def test-section "> foo\r\n> bar\r\n> baz\r\nwibble\r\n1. hola!\r\n 2.Hello!\r\n 3. rgr")
+(def test-section "> foo\r\n> bar\r\n> baz\r\ntest2\r\n 1. #lol!\r\n2. k\r\nxor")
+(def test-section-1 "> foo\r\n> bar\r\n> baz\r\nwibble\r\n1. hola!\r\n 2.Hello!\r\n 3. rgr")
 
 (defn -main
   []
