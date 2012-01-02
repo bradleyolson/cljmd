0015125cbea5ee3b53fe9632c01a3063ab9ac158
Brad Olson <alllateralus@gmail.com>
2012-01-01 23:57:35 -0600

    reworked to start generalizing strings

--- src/cljmd/core.clj
+++ src/cljmd/core.clj
@@ -1,18 +1,41 @@
 (ns cljmd.core
   (:require [clojure.contrib.jmx :as jmx])
-  (:use clojure.string))
+  (:require clojure.string))
 
 (def block-opts
  { 
     :blockquote #"^[\ ]{0,1}\>\ "
     :ol #"^[\ ]{0,1}[0-9]*\." 
   })
-
-;(def style-opts "foo")
+(def anchor-keys {})
 
 (defn list-to-text
   [text]
   (apply str (interpose "\r\n" text)))
+
+(defn build-tag
+  [content tag]
+  (let [tag-name (if-not (nil? tag) tag "p")]
+    (if-not (empty content)
+      (str "<" tag-name ">" content "</" tag-name ">"))))
+
+(defn remove-md
+  [section tag opts & replacer]
+  (let [replace-with (if-not (empty (first replacer)) (first replacer) "")
+        lines (split-lines section)
+        content (map (fn [line] 
+                   (clojure.string/replace line (tag opts) replace-with)) 
+                 lines)]
+    (println (map (fn [line] 
+                   (clojure.string/replace-re (tag opts) replace-with line))
+                 lines))
+      content))
+
+(defn sectionalize
+  [^String text]
+  (seq (.split #"\r?\n\r?\n" text)))
+
+; ---
 
 (defn get-block
   [line opts]
@@ -21,12 +44,12 @@
 
 (defn build-block
   [line prev nex tag opts]
-  (print line ": " prev)
   (let [reg (opts tag)]
-    (if (and (not (nil? prev)) (not-empty (re-find reg prev)))
-      ))
+    (if-not (and (nil? prev) (re-find reg prev))
+      (println "foo")
+      (println "bar")) 
   ;(if (not (and (nil? prev) (re-find (opts tag) prev))))
-  (str "built block! " line " for " tag))
+  (str "built block! " line " for " tag)))
 
 (defn check-sections
   [lines & prev]
@@ -34,29 +57,40 @@
         prev-line (first prev)
         next-line (first (rest lines)) 
         curr-line (first lines)
-        line (if-not (nil? block-type))
+        line (if-not (nil? block-type)
                (build-block curr-line prev-line next-line block-type block-opts)
-               (first lines)]
+               (first lines))]
       (cons line (if (not-empty (rest lines)) (check-sections (rest lines) curr-line)))))
 
-(defn sectionalize
+(defn build-blocks
   [lines]
   (let [sections (check-sections lines)]
     (if (not= sections lines)
       (recur sections)
       sections)))
 
+; ---
+
+(defn section-markup
+  [section]
+   (map (fn [sect]
+          (let [opts block-opts
+                block-type (get-block sect opts)]
+            (if-not (nil? block-type)
+              (do
+                (build-tag (remove-md sect block-type opts) block-type) 
+                )
+                (build-tag sect "p"))))
+        section))
+
 (defn build-markup
   [text]
-  (let [lines (split-lines text)]
+  (let [sections (section-markup (sectionalize text))]
     (println "running markup builder")
-    (println (list-to-text (sectionalize lines)))
-    (sectionalize lines)
+    (println (list-to-text sections))
     (println "ending build")))
 
 (defn -main
   []
   (do
     (println "ran main...\r\n")))
-
-
--- src/cljmd/test.clj
+++ src/cljmd/test.clj
@@ -3,17 +3,19 @@
   (:require [cljmd.old :as old]))
 
 (def test-text
-"foo
+"> foo
+
 #bar
 ###bazw#ibble\r\n# wobble
+
 #######flub")
 
-(def test-section "> foo\r\n> bar\r\n> baz\r\ntest2\r\n 1. #lol!\r\n2. k")
+(def test-section "> foo\r\n\r\n> bar\r\n> baz\r\ntest2\r\n 1. #lol!\r\n2. k")
 (def test-section-1 "> foo\r\n> bar\r\n> baz\r\nwibble\r\n1. hola!\r\n 2.Hello!\r\n 3. rgr")
 
 (defn -main
   []
   (do
     ;(println (old/build-markup test-section))
-    (println (core/build-markup test-section))
+    (println (core/build-markup test-text))
     ))
